7f3bcb865f74058ee360c0bff849f7fca7bc116d
Hal Dunn <heyhal19@gmail.com>
2021-01-21 12:10:38 -0800

    formatting for login form fixed

--- src/App.js
+++ src/App.js
@@ -26,9 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
         <Login />
-      </header>
     </div>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,54 +1,64 @@
 import React from 'react'
-import { Form, Input, Button, InputNumber } from 'antd'
+import { Form, Input, Button, InputNumber, Layout, Breadcrumb, Menu } from 'antd'
+const { Header, Content, Footer } = Layout;
+
 
 const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+    labelCol: { span: 6 },
+    wrapperCol: { span: 12 },
 }
 const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
+    wrapperCol: { offset: 6, span: 12 }
 }
 
 
 function Login() {
     const [form] = Form.useForm()
 
-    // make it so age can't go negative or over 150
-
 
     return (
-        <>
-            <Form
-                onFinish={(eventInfo) => console.log("object", eventInfo)}
-                {...layout}
-                label="login"
-                form={form}>
-                <Form.Item
-                    label='Username'
-                    name='username'
-                    rules={[{ required: true, message: "Username is required" }]}>
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label='Email'
-                    name='email'
-                    rules={[{ required: true, message: "Email is required" }]}>
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label='Age:'
-                    name='age'
+        <Layout className='layout'>
+            <Header style={{color: "white"}}>
+                hot girl shit
+            </Header>
+            <Content>
+                <Form
+                    onFinish={(eventInfo) => console.log("object", eventInfo)}
+                    {...layout}
+                    label="login"
+                    form={form}
+                    style={{paddingTop: "24px"}}>
+                    <Form.Item
+                        label='Username'
+                        name='username'
+                        rules={[{ required: true, message: "Username is required" }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label='Email'
+                        name='email'
+                        rules={[{ required: true, message: "Email is required" }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label='Age'
+                        name='age'
                     >
-                    <InputNumber style={{color: "white"}}/>
-                </Form.Item>
-                <Form.Item {...tailLayout}>
-                    <Button type='primary' htmlType='submit'>Login</Button>
-                    {" "}
-                    <Button>Or Signup</Button>
-                </Form.Item>
-            </Form>
-        </>
+                        <InputNumber />
+                    </Form.Item>
+                    <Form.Item {...tailLayout}>
+                        <Button type='primary' htmlType='submit'>Login</Button>
+                        {" "}
+                        <Button>Or Signup</Button>
+                    </Form.Item>
+                </Form>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}> Copyright Hal Dunn 2021 Don't Steal This Amazing Website Or Else</Footer>
+        </Layout>
     )
 }
 
 export default Login
+
+
+// make it so age can't go negative or over 150
